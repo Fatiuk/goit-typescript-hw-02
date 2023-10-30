656f344be3ca3b34bfd086bffd280b869e49ebf5
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-10-30 22:26:01 +0200

    feat: [generic] update task 4

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,16 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
-  }
+interface ComponentProps {
+  title: string;
 }
 
-class Page extends Component {
-  pageInfo () {
+class Component<T> {
+  constructor(public props: T) {}
+}
+
+class Page extends Component<ComponentProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
