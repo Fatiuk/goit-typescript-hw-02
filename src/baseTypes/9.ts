16faf5d999e885f6ed130288c18cdaed833a9852
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-10-30 19:10:43 +0200

    feat: [baseTypes] complete task 9

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,22 +2,33 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
+type Pages = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: string;
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
+};
+
 const page1 = {
-  title: 'The awesome page',
+  title: "The awesome page",
   likes: 100,
-  accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
   details: {
-    createAt: new Date('2021-01-01'),
-    updateAt: new Date('2021-05-01'),
-  }
-}
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
 
 const page2 = {
-  title: 'Python or Js',
+  title: "Python or Js",
   likes: 5,
-  accounts: ['Alex'],
-  status: 'close',
-}
+  accounts: ["Alex"],
+  status: "close",
+};
 
 export {};
