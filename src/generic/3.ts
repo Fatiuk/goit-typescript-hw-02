37751e5cd4f30d89b130f7da4d13a38859c04ce1
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-10-30 22:17:42 +0200

    feat: [generic] update task 3

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,8 +3,8 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge (objA, objB) {
+function merge<T extends {}, U extends {}>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-export {}
+export {};
