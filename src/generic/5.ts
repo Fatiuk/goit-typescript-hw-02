0e17dfde4fcf6b2d5ccdf425ea724ce9390ea778
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-10-31 12:05:38 +0200

    feat: [generic] update task 5

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,9 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<T, U> {
+  key: T;
+  value: U;
 }
 
 export {};
